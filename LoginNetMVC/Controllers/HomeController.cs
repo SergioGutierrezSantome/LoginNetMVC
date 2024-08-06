ad0e1d2d3a0d6db3238f245e1166279e1e57c933
SergioGutierrezSantome <gutierrez.sergio.360@gmail.com>
2024-08-06 12:35:18 +0200

    Agregar archivos de proyecto.

---
+++ LoginNetMVC/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoginNetMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {   return View();
+           
+        }
+
+        public ActionResult login()
+        {
+            return View();
+        }
+       
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult LoginForm(string username, string password)
+        {
+            if (username == "admin" && password == "1234")
+            {
+                return View("Index");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Username or password is incorrect.");
+            }
+            return login() ;
+        }
+    }
+}
---
+++ LoginNetMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LoginNetMVC.MvcApplication" Language="C#" %>
